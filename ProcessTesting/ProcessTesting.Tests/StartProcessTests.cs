f38042f9b77ec17a735bdcf6bc03832c44a6f753
Anna Sojka-Pal <anna.sojka@soneta.pl>
2021-09-24 15:00:34 +0200

    Start manualny procesu

--- ProcessTesting/ProcessTesting.Tests/StartProcessTests.cs
+++ ProcessTesting/ProcessTesting.Tests/StartProcessTests.cs
@@ -1,36 +1,44 @@
-﻿using Soneta.Test;
+﻿using System;
+using Soneta.Test;
 using System.Reflection;
 using NUnit.Framework;
 using Soneta.Workflow;
 using FluentAssertions;
+using Soneta.Business;
+using Soneta.Core.DbTuples;
+using Soneta.Test.Helpers.Extensions;
+using Soneta.Workflow.Config;
 using Soneta.Workflow.Dms;
 using static ProcessTesting.Tests.Tools;
 
 namespace ProcessTesting.Tests
 {
     [TestDatabasePlatynowa]
-    internal class StartProcessTests : TestBase
+    internal class StartProcessTests : DbTransactionTestBase
     {
         public override void ClassSetup()
         {
             // Konieczne jest wczytanie assembly dodatku
-            Assembly.Load("ProcessTesting");
+            LoadAssembly("ProcessTesting");
             base.ClassSetup();
         }
 
         [Test]
         public void ShouldAutomaticallyStartProcessTest() {
             var wfDefGuid = Guids[Names.WfTest];
-            SetDefinition(this, wfDefGuid, "WfTest.xml");
-            var wfDef = Session.GetWorkflow().WFDefs[wfDefGuid];
-            wfDef.Should().NotBeNull();
+            var wfDef = ImportWfDefinition(wfDefGuid, "WfTest.xml");
 
             AddBasicDocument();
 
-            wfDef = Get(wfDef);
-            var processes = Session.GetWorkflow().WFWorkflows.WgWorkflowDefinition[wfDef];
-            processes.Should().NotBeEmpty();
-            processes.Count.Should().Be(1);
+            AssertStartedProcess(Get(wfDef));
+        }
+
+        private WFDefinition ImportWfDefinition(Guid wfDefGuid, string resourceName) {
+            SetDefinition(this, wfDefGuid, resourceName);
+            var wfDef = Session.GetWorkflow().WFDefs[wfDefGuid];
+            wfDef.Should().NotBeNull();
+
+            return wfDef;
         }
 
         private void AddBasicDocument() {
@@ -43,6 +51,36 @@ namespace ProcessTesting.Tests
             Add(new BasicDocument(definition, register));
 
             SaveDispose();
+        }
+
+        private void AssertStartedProcess(WFDefinition wfDef) {
+            var processes = Session.GetWorkflow().WFWorkflows.WgWorkflowDefinition[wfDef];
+            processes.Should().NotBeEmpty();
+            processes.Count.Should().Be(1);
+        }
+
+        [Test]
+        public void ShouldManuallyStartProcessTest() {
+            var wfDefGuid = Guids[Names.CostLetterProcess];
+            var wfDef = ImportWfDefinition(wfDefGuid, "DokKosztowy.xml");
+            SetRightsOnTupleDef();
+
+            InUITransaction(() => {
+                var cx = Context.Empty.Clone(Session);
+                WorkflowTools.StartWorkflowProcess(wfDef, true,
+                    Session.CurrentTransaction,
+                    null, ref cx, out var taskCreated);
+                taskCreated.Should().BeTrue();
+            });
+            SaveDispose();
+
+            AssertStartedProcess(Get(wfDef));
+        }
+
+        private void SetRightsOnTupleDef() {
+            var dbTupleDef = GetConfig<DbTupleDefinition>(Guids[Names.CostLetterTupleDef]);
+            dbTupleDef.Should().NotBeNull();
+            dbTupleDef.SetRightSource(ConfigEditSession, AccessRights.Granted, SaveDisposeConfig);
         }
     }
 }
