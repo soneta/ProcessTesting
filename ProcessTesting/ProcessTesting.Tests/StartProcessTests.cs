e37bf28d3c7fec372eb5ca85f13429809df6cc84
Anna Sojka-Pal <anna.sojka@soneta.pl>
2021-09-24 07:52:10 +0200

    Inicjacja repozytorium

---
+++ ProcessTesting/ProcessTesting.Tests/StartProcessTests.cs
@@ -0,0 +1,16 @@
+﻿using Soneta.Test;
+using System.Reflection;
+
+namespace ProcessTesting.Tests
+{
+    [TestDatabasePlatynowa]
+    internal class StartProcessTests : TestBase
+    {
+        public override void ClassSetup()
+        {
+            // Konieczne jest wczytanie assembly dodatku
+            Assembly.Load("ProcessTesting");
+            base.ClassSetup();
+        }
+    }
+}
